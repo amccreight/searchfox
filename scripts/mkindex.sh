730b73898fa6aca4b80b40b5c91c55186a58469d
Andrew McCreight <continuation@gmail.com>
2017-11-08 23:41:51 +0000

    remove non C++ stuff from mkindex.sh

--- scripts/mkindex.sh
+++ scripts/mkindex.sh
@@ -20,40 +20,8 @@ export PYTHONPATH=$MOZSEARCH_PATH/scripts
 
 date
 
-$CONFIG_REPO/$TREE_NAME/find-repo-files $CONFIG_FILE $TREE_NAME
-$MOZSEARCH_PATH/scripts/mkdirs.sh
-
-date
-
-$CONFIG_REPO/$TREE_NAME/build || echo "Build failed: $TREE_NAME"
-
-date
-
 $MOZSEARCH_PATH/scripts/find-objdir-files.py
-$MOZSEARCH_PATH/scripts/objdir-mkdirs.sh
+#$MOZSEARCH_PATH/scripts/objdir-mkdirs.sh
 
-date
-
-$MOZSEARCH_PATH/scripts/js-analyze.sh $CONFIG_FILE $TREE_NAME
-
-date
-
-$MOZSEARCH_PATH/scripts/idl-analyze.sh $CONFIG_FILE $TREE_NAME
-
-date
-
-$MOZSEARCH_PATH/scripts/ipdl-analyze.sh $CONFIG_FILE $TREE_NAME
-
-date
-
+echo CROSS REF
 $MOZSEARCH_PATH/scripts/crossref.sh $CONFIG_FILE $TREE_NAME
-
-date
-
-$MOZSEARCH_PATH/scripts/output.sh $CONFIG_REPO $CONFIG_FILE $TREE_NAME
-
-date
-
-$MOZSEARCH_PATH/scripts/build-codesearch.py $CONFIG_FILE $TREE_NAME
-
-date
